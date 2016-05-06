bab405b1829b2c830a319b797ded63101f9a20eb
RoggerFabri <theblind@gmail.com>
2016-05-05 22:50:34 -0300

    Creating Repository

---
+++ FolderSplitter/DefaultForm.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace FolderSplitter
+{
+	public partial class DefaultForm : Form
+	{
+		public IList<string> Files;
+		public string SourceDirectory;
+		public int QuantityOfFiles = 100;
+		private BackgroundWorker _moveFilesBackgroundWorker;
+
+		public DefaultForm()
+		{
+			InitializeComponent();
+		}
+
+		private void openFolderButton_Click(object sender, EventArgs e)
+		{
+			if (folderDialog.ShowDialog() == DialogResult.OK)
+			{
+				startButton.Enabled = true;
+				SourceDirectory = folderDialog.SelectedPath;
+				Files = Directory.GetFiles(SourceDirectory);
+			}
+			else
+			{
+				startButton.Enabled = false;
+			}
+		}
+
+		private void startButton_Click(object sender, EventArgs e)
+		{
+			var movedFiles = 0;
+			var numOfDirectories = (int)Math.Ceiling((double)Files.Count / QuantityOfFiles);
+			CreateSubDirectories(numOfDirectories);
+
+			ProgressBarBackgroundWorker(progressBar, OnComplete);
+			_moveFilesBackgroundWorker.RunWorkerAsync(new MethodInvoker(() =>
+			{
+				progressBar.BeginInvoke(new MethodInvoker(() => progressBar.Visible = true));
+				try
+				{
+					for (var i = 0; i < numOfDirectories; i++)
+					{
+						var portionOfFiles = Files.OrderBy(f => f).Skip(i * QuantityOfFiles).Take(QuantityOfFiles);
+						foreach (var file in portionOfFiles)
+						{
+							File.Move(file, Path.Combine(SourceDirectory, (i + 1).ToString(), Path.GetFileName(file)));
+							movedFiles++;
+							var progress = (double)movedFiles / Files.Count * 100;
+							_moveFilesBackgroundWorker.ReportProgress((int)Math.Truncate(progress));
+						}
+					}
+				}
+				catch (Exception ex) { }
+			}));
+		}
+
+		private void OnComplete()
+		{
+			Application.Exit();
+		}
+
+		private void CreateSubDirectories(int quantity)
+		{
+			for (var i = 1; i <= quantity; i++)
+				Directory.CreateDirectory(Path.Combine(SourceDirectory, i.ToString()));
+		}
+
+		private void ProgressBarBackgroundWorker(ProgressBar progressBar, Action callBack)
+		{
+			_moveFilesBackgroundWorker = new BackgroundWorker();
+			_moveFilesBackgroundWorker.WorkerReportsProgress = true;
+			_moveFilesBackgroundWorker.DoWork += (sender, e) => ((MethodInvoker)e.Argument).Invoke();
+			_moveFilesBackgroundWorker.ProgressChanged += (sender, e) =>
+			{
+				progressBar.Style = ProgressBarStyle.Continuous;
+				progressBar.Value = e.ProgressPercentage;
+			};
+			_moveFilesBackgroundWorker.RunWorkerCompleted += (sender, e) =>
+			{
+				callBack();
+			};
+		}
+	}
+}
---
+++ FolderSplitter/defaultForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace FolderSplitter
+{
+	partial class DefaultForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.defalultPanel = new System.Windows.Forms.Panel();
+			this.progressBar = new System.Windows.Forms.ProgressBar();
+			this.startButton = new System.Windows.Forms.Button();
+			this.openFolderButton = new System.Windows.Forms.Button();
+			this.folderDialog = new System.Windows.Forms.FolderBrowserDialog();
+			this.defalultPanel.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// defalultPanel
+			// 
+			this.defalultPanel.Controls.Add(this.progressBar);
+			this.defalultPanel.Controls.Add(this.startButton);
+			this.defalultPanel.Controls.Add(this.openFolderButton);
+			this.defalultPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.defalultPanel.Location = new System.Drawing.Point(0, 0);
+			this.defalultPanel.Name = "defalultPanel";
+			this.defalultPanel.Size = new System.Drawing.Size(284, 61);
+			this.defalultPanel.TabIndex = 0;
+			// 
+			// progressBar
+			// 
+			this.progressBar.Location = new System.Drawing.Point(3, 34);
+			this.progressBar.Name = "progressBar";
+			this.progressBar.Size = new System.Drawing.Size(278, 23);
+			this.progressBar.TabIndex = 3;
+			// 
+			// startButton
+			// 
+			this.startButton.Enabled = false;
+			this.startButton.Location = new System.Drawing.Point(206, 3);
+			this.startButton.Name = "startButton";
+			this.startButton.Size = new System.Drawing.Size(75, 25);
+			this.startButton.TabIndex = 2;
+			this.startButton.Text = "Start";
+			this.startButton.UseVisualStyleBackColor = true;
+			this.startButton.Click += new System.EventHandler(this.startButton_Click);
+			// 
+			// openFolderButton
+			// 
+			this.openFolderButton.Image = global::FolderSplitter.Properties.Resources.folder_explore;
+			this.openFolderButton.Location = new System.Drawing.Point(3, 3);
+			this.openFolderButton.Name = "openFolderButton";
+			this.openFolderButton.Size = new System.Drawing.Size(25, 25);
+			this.openFolderButton.TabIndex = 0;
+			this.openFolderButton.UseVisualStyleBackColor = true;
+			this.openFolderButton.Click += new System.EventHandler(this.openFolderButton_Click);
+			// 
+			// DefaultForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 61);
+			this.Controls.Add(this.defalultPanel);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.Name = "DefaultForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "FolderSplitter";
+			this.defalultPanel.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel defalultPanel;
+		private System.Windows.Forms.Button openFolderButton;
+		private System.Windows.Forms.FolderBrowserDialog folderDialog;
+		private System.Windows.Forms.ProgressBar progressBar;
+		private System.Windows.Forms.Button startButton;
+	}
+}
+
